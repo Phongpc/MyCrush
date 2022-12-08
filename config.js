3b1f7a6786d0a3419427b5e370bbd9bce39c3195
Nguyễn Duy Phong <devphongpc@gmail.com>
2022-12-08 23:41:50 +0700

    source code

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "...",
+    introTitle: '...',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cậu cứ đi nhầm đường.
+    Lạc vào tim tớ lẻ loi.
+    Đằng sau chữ yêu đấy là thương`,
+    btnIntro: '^^Cậu bấm vào đây đi^^',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại chọn mình <3',
+    btnReply: 'Gửi cho Phong <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Mai mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100073628621847'
+}
+0
